01ca014718dafed5d80c019b9a774d260ad56222
MidhunSMukundan <103759733+MidhunSMukundan@users.noreply.github.com>
2023-10-11 00:40:11 +0400

    s3 and html added

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "bucket_name" {
   description = "buscket name for static website hosting"
   value       = module.terrahouse_aws.bucket_name
 }
+
+
+
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.website_configuration.website_endpoint
+}
